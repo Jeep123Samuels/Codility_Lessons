435a4d7ea06436b1f5cc2c8b5daab06c89c1e6f3
Jeep123Samuels <jeep123samuels@gmail.com>
2018-05-11 15:59:01 +0200

    refactor(lesson_one)add better solution to binary_gaps

--- lesson_1/binary_gaps.py
+++ lesson_1/binary_gaps.py
@@ -14,29 +14,16 @@
 import random
 
 
-def solution(integer_str):
+def solution(binary_str):
     """Returns maximum binary gap for a given binary string."""
-    binary_gaps = []
-    previous_index = None
-    for idx, x in enumerate(integer_str):
-        idx_diff = 0
-        # Check if current character == 1
-        if x == '1':
-            # Don't calculate index difference if it is first '1'
-            if previous_index is not None:
-                idx_diff = (idx - previous_index) - 1
-            # Skip append of index difference if zero.
-            if idx_diff > 0:
-                binary_gaps.append(idx_diff)
-            # Set current index of '1' to previous index.
-            previous_index = idx
+    binary_gaps = [len(x) for x in binary_str.rstrip('0').rstrip('0').split('1')]
     return max(binary_gaps) if binary_gaps else 0
 
 
 if __name__ == "__main__":
     for _ in range(10):
         i = random.randint(200, 647)
-        binary_str = str(bin(i))[2:]
+        gaps = solution(bin(i)[2:])
         print('i : {0}'.format(i))
-        print('i binary : {0}'.format(binary_str))
-        print('solution: {0}\n'.format(solution(binary_str)))
+        print('i binary : {0}'.format(bin(i)[2:]))
+        print('solution: {0}\n'.format(gaps))
