9c264bc43d1a21de7007fa1aeb06a1d8b51f3516
Jeep123Samuels <jeep123samuels@gmail.com>
2018-05-14 11:32:26 +0200

    feat(odd_occurrence): add lesson 2 odd occurrence

--- lesson_2/tests.py
+++ lesson_2/tests.py
@@ -3,6 +3,7 @@
 import pytest
 
 from lesson_2.array_rotation import solution as array_rotation_solution
+from lesson_2.odd_occurrence import solution as odd_occurrence_solution
 
 
 @pytest.mark.parametrize('array_test,rotations,expected_results', (
@@ -16,3 +17,18 @@ from lesson_2.array_rotation import solution as array_rotation_solution
 def test_array_rotation(array_test, rotations, expected_results):
     """Should rotate arrays as expected."""
     assert array_rotation_solution(array_test, rotations) == expected_results
+
+
+@pytest.mark.parametrize('array_test,expected_results', (
+    ([], 0),
+    ([1, 2, 1], 2),
+    ([1, 2, 3, 2, 1], 3),
+    ([5, 1, 3, 1, 2, 2, 3], 5),
+    ([1, 7, 1, 1, 1], 7),
+    ([1, 1, 2, 10, 8, 2, 8], 10),
+    ([1, 1, 2, 8, 2, 8], 0),
+    ([9, 3, 9, 3, 9, 7, 9], 7),
+))
+def test_odd_occurrence_in_array(array_test, expected_results):
+    """Should return the value odd occurring in an array."""
+    assert odd_occurrence_solution(array_test) == expected_results
