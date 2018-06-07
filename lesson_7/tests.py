8b6dc1aea6e23b07ece0cc28dcaf2a98a96a6f53
Jeep123Samuels <jeep123samuels@gmail.com>
2018-06-07 13:36:12 +0200

    feat(lesson_7): add binary_fish solution

--- lesson_7/tests.py
+++ lesson_7/tests.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from lesson_7.fish_swimming import solution as fish_swimming_solution
 from lesson_7.nested_brackets import solution as nested_brackets_solution
 
 
@@ -24,3 +25,30 @@ def test_nested_brackets_solution_properly_validates_strings(
     # when ... solution function called
     # then ... returned expected valuation
     assert nested_brackets_solution(test_string) == expected_bool
+
+
+@pytest.mark.parametrize('array_A,array_B,expected_array', (
+    ([4, 3, 2, 1, 5], [1, 1, 1, 1, 0], [5]),
+    ([4, 3, 2, 1, 5], [1, 0, 0, 0, 0], [5]),
+    ([4, 3, 2, 1, 5], [0, 1, 1, 1, 0], [4, 5]),
+    ([4, 3, 2, 1, 5], [0, 1, 0, 1, 0], [4, 5]),
+    ([4, 3, 2, 1, 5], [0, 1, 0, 0, 0], [4, 5]),
+    ([4, 3, 2, 1, 5], [0, 0, 1, 0, 0], [4, 3, 5]),
+    ([4, 3, 2, 1, 5], [0, 0, 0, 1, 0], [4, 3, 2, 5]),
+    ([4, 3, 2, 1, 5], [0, 0, 0, 0, 1], [4, 3, 2, 1, 5]),
+    ([4, 3, 2, 1, 5], [1, 1, 1, 1, 1], [4, 3, 2, 1, 5]),
+    (
+        [4, 3, 2, 1, 5, 7, 9, 15, 6, 8, 20, 11],
+        [0, 0, 0, 0, 1, 1, 1,  1, 1, 1,  1,  1],
+        [4, 3, 2, 1, 5, 7, 9, 15, 6, 8, 20, 11],
+    ),
+))
+def test_correctly_filters_array_with_remaining_numbers(
+    array_A,
+    array_B,
+    expected_array,
+):
+    """Should return array of the remaining values in the array."""
+    # when ... solution function called
+    # then ... returned expected array
+    assert fish_swimming_solution(array_A, array_B) == expected_array
